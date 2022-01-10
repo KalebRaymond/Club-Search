3812aad721e48f1d81397615de89d16e8bde8677
KR <k.raymond.form@gmail.com>
2022-01-09 19:50:14 -0500

    Basic layout for angular search page

--- club-search/src/app/app.module.ts
+++ club-search/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SearchPanelComponent } from './search-panel/search-panel.component';
+import { ClubsContainerComponent } from './clubs-container/clubs-container.component';
+import { ClubCardComponent } from './club-card/club-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchPanelComponent,
+    ClubsContainerComponent,
+    ClubCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ club-search/src/app/clubs-container/clubs-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clubs-container',
+  templateUrl: './clubs-container.component.html',
+  styleUrls: ['./clubs-container.component.scss']
+})
+export class ClubsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
